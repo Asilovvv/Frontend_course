c8f829c53fe6cbf911b6aa45bcd18321f82d6fe4
Asilovvv <bekturasilov@gmail.com>
2024-11-20 20:46:10 +0600

    Create myInfo component and display user information

---
+++ react-project/src/myInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const myInfo = () => {
+  return (
+    <div>
+      <h1>Bektur Asilov</h1>
+      <p>Position: Software Engineer</p>
+      <p>Company: Apple</p>
+      <p>Location: Bishkek, Kyrgyzstan</p>
+      <p>Email: bekaasilov75@gmail.com</p>
+      <p>Phone: +996755010210</p>
+    </div>
+  );
+};
+
+export default myInfo;
