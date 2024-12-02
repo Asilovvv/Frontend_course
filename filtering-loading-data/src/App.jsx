416237e31848a4a30c5d969e0442acccd8c9db76
Asilovvv <bekturasilov@gmail.com>
2024-12-02 15:00:10 +0600

    Initialized data list on first render

--- filtering-loading-data/src/App.jsx
+++ filtering-loading-data/src/App.jsx
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [data, setData] = useState([]); // Original data list
-  const [searchTerm, setSearchTerm] = useState(""); // Search string
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      const sampleData = [
+        { id: 1, name: "Ferrari" },
+        { id: 2, name: "Lamborghini" },
+        { id: 3, name: "Porshe" },
+        { id: 4, name: "Bugatti" },
+        { id: 5, name: "Aston Martin" },
+        { id: 6, name: "BMW" },
+        { id: 7, name: "Audi" },
+        { id: 8, name: "Masserati" },
+        { id: 9, name: "Nissan" },
+        { id: 10, name: "Cadillac" },
+      ];
+      setData(sampleData);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
